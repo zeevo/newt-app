485f07001e9dd28499298877485d81db1b65109e
zeevo <8338013+zeevo@users.noreply.github.com>
2026-04-05 22:01:07 -0400

    improve example

--- packages/templates/src/root/templates/env-example.ts
+++ packages/templates/src/root/templates/env-example.ts
@@ -1,6 +1,6 @@
 export default {
   filename: ".env",
-  template: `DATABASE_URL=postgresql://user:password@localhost:5432/dbname
+  template: `DATABASE_URL=postgresql://postgres:postgres@localhost:5432/postgres
 BETTER_AUTH_URL=http://localhost:3000
 BETTER_AUTH_SECRET=your-secret-here`,
 };
