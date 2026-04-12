a8bc4cfcc3b3f07889528c33e0a0cab0ac89d29e
zeevo <8338013+zeevo@users.noreply.github.com>
2026-04-11 21:09:46 -0400

    add app.service.ts, rename getNestApp to getContext, wire AppService into hello route

--- packages/templates/src/api-controllers/templates/app-controller.ts
+++ packages/templates/src/api-controllers/templates/app-controller.ts
@@ -6,9 +6,12 @@ import {
   Session,
   UserSession,
 } from '@thallesp/nestjs-better-auth';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @AllowAnonymous()
   @Get('hello')
   getHello(@Session() session: UserSession | null) {
@@ -16,7 +19,7 @@ export class AppController {
       return { message: \`Hello \${session.user.name}\` };
     }
 
-    return { message: 'Hello from Nest' };
+    return { message: this.appService.getHello() };
   }
 }`,
 };
