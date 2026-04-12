23cccd89b177b25ac3f98f57634b601e0af0e504
zeevo <8338013+zeevo@users.noreply.github.com>
2026-04-11 20:59:47 -0400

    simplify nest context to module-level promise

--- packages/templates/src/nest-di-only/templates/web-nest-context.ts
+++ packages/templates/src/nest-di-only/templates/web-nest-context.ts
@@ -5,14 +5,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@<%= projectName %>/api';
 import type { INestApplicationContext } from '@nestjs/common';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var __nestApp: Promise<INestApplicationContext> | undefined;
-}
-
-global.__nestApp ??= NestFactory.createApplicationContext(AppModule, { logger: false });
+const nestApp: Promise<INestApplicationContext> = NestFactory.createApplicationContext(AppModule, { logger: false });
 
 export function getNestApp(): Promise<INestApplicationContext> {
-  return global.__nestApp!;
+  return nestApp;
 }`,
 };
