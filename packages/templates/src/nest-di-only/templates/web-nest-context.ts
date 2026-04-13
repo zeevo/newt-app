cd49cd3ec7e25f3ae005fa4341159c6422274f79
zeevo <8338013+zeevo@users.noreply.github.com>
2026-04-13 00:26:28 -0400

    add inject helper to nest di-only context

--- packages/templates/src/nest-di-only/templates/web-nest-context.ts
+++ packages/templates/src/nest-di-only/templates/web-nest-context.ts
@@ -3,7 +3,7 @@ export default {
   template: `import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@<%= projectName %>/api';
-import type { INestApplicationContext } from '@nestjs/common';
+import type { INestApplicationContext, Type, Abstract } from '@nestjs/common';
 
 let context: INestApplicationContext | null = null;
 
@@ -14,5 +14,10 @@ export async function getContext(): Promise<INestApplicationContext> {
     });
   }
   return context;
+}
+
+export async function inject<T>(token: Type<T> | Abstract<T> | string | symbol): Promise<T> {
+  const ctx = await getContext();
+  return ctx.get<T>(token);
 }`,
 };
