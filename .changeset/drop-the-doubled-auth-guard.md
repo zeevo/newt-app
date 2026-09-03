0665f33d23bc9b7e2283f7c6f8ded308969b7dcb
Shane O'Neill <8338013+zeevo@users.noreply.github.com>
2026-09-02 23:58:32 -0400

    drop the doubled auth guard (#433)

---
+++ .changeset/drop-the-doubled-auth-guard.md
@@ -0,0 +1,5 @@
+---
+"create-newt-app": patch
+---
+
+drop the APP_GUARD provider the nest better-auth module already registers
--- packages/create-newt-app/src/templates/api-controllers/templates/app-module.ts
+++ packages/create-newt-app/src/templates/api-controllers/templates/app-module.ts
@@ -4,9 +4,8 @@ export default {
   filename: "apps/api/src/app.module.ts",
   template: `<% if (deployment === 'spa') { %>import { join } from 'path';
 <% } %>import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
 <% if (deployment === 'spa') { %>import { ServeStaticModule } from '@nestjs/serve-static';
-<% } %>import { AuthGuard, AuthModule } from '@thallesp/nestjs-better-auth';
+<% } %>import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { auth } from '@<%= projectName %>/auth';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -23,7 +22,7 @@ import { AppService } from './app.service';
 <% } else { %>  imports: [AuthModule.forRoot({ auth })],
 <% } -%>
   controllers: [AppController],
-  providers: [AppService, { provide: APP_GUARD, useClass: AuthGuard }],
+  providers: [AppService],
 })
 export class AppModule {}`,
 };
