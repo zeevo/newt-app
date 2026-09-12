5253aa24a028bd0af6e9934e806ce5733b54268c
Shane O'Neill <8338013+zeevo@users.noreply.github.com>
2026-09-12 00:24:29 -0400

    404 missing files in spa mode instead of serving index.html (#482)

--- packages/create-newt-app/src/templates/todo-example/templates/app-module-controllers.ts
+++ packages/create-newt-app/src/templates/todo-example/templates/app-module-controllers.ts
@@ -16,7 +16,7 @@ import { TodosModule } from './todos/todos.module';
     // serves apps/web's static export; the api runs from apps/api
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), '../web/out'),
-      exclude: ['/api/{*splat}'],
+      exclude: ['/api/{*splat}', '/{*path}.:ext'],
     }),
     AuthModule.forRoot({ auth }),
     TodosModule,
